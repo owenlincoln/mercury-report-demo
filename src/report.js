9f9d5435ea5fa65e4ecfb1c69201f25d6b6c837b
owen.lincoln <owen.lincoln@BKTSFL141>
2018-11-13 17:15:58 -0800

    paging

---
+++ src/report.js
@@ -0,0 +1,216 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
+
+const CustomTableCell = withStyles(theme => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+
+const actionsStyles = theme => ({
+  root: {
+    flexShrink: 0,
+    color: theme.palette.text.secondary,
+    marginLeft: theme.spacing.unit * 2.5,
+  },
+});
+
+class TablePaginationActions extends React.Component {
+  handleFirstPageButtonClick = event => {
+    this.props.onChangePage(event, 0);
+  };
+
+  handleBackButtonClick = event => {
+    this.props.onChangePage(event, this.props.page - 1);
+  };
+
+  handleNextButtonClick = event => {
+    this.props.onChangePage(event, this.props.page + 1);
+  };
+
+  handleLastPageButtonClick = event => {
+    this.props.onChangePage(
+      event,
+      Math.max(0, Math.ceil(this.props.count / this.props.rowsPerPage) - 1),
+    );
+  };
+
+  render() {
+    const { classes, count, page, rowsPerPage, theme } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <IconButton
+          onClick={this.handleFirstPageButtonClick}
+          disabled={page === 0}
+          aria-label="First Page"
+        >
+          {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+        </IconButton>
+        <IconButton
+          onClick={this.handleBackButtonClick}
+          disabled={page === 0}
+          aria-label="Previous Page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        </IconButton>
+        <IconButton
+          onClick={this.handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="Next Page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        </IconButton>
+        <IconButton
+          onClick={this.handleLastPageButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="Last Page"
+        >
+          {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+        </IconButton>
+      </div>
+    );
+  }
+}
+
+TablePaginationActions.propTypes = {
+  classes: PropTypes.object.isRequired,
+  count: PropTypes.number.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+const TablePaginationActionsWrapped = withStyles(actionsStyles, { withTheme: true })(
+  TablePaginationActions,
+);
+
+let id = 0;
+function createData(sponsor, study, from, to, completed, completed_us, completed_int, project_code) {
+  id += 1;
+  return { id, sponsor, study, from, to, completed, completed_us, completed_int, project_code };
+}
+
+const data = [
+    createData('Acerta', 'ACE-LY-003', '7 Nov 2018', '8 Nov 2018', 45, 45, 24, 1565),
+    createData('Genetech', 'GO40782', '7 Nov 2018', '8 Nov 2018', 10, 5, 5, 6762),
+    createData('Gilead', 'GS-US-427-4024', '7 Nov 2018', '8 Nov 2018', 78, 40, 38, 5635),
+    createData('Janssen', 'CNTO1275CRD3005', '7 Nov 2018', '8 Nov 2018', 9, 6, 3, 7645),
+  ];
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+  },
+  table: {
+    minWidth: 500,
+  },
+  tableWrapper: {
+    overflowX: 'auto',
+  },
+});
+
+class CustomPaginationActionsTable extends React.Component {
+  state = {
+    rows: data.sort((a, b) => (a.sponsor < b.sponsor ? -1 : 1)),
+    page: 0,
+    rowsPerPage: 10,
+  };
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { rows, rowsPerPage, page } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+    return (
+      <Paper className={classes.root}>
+        <div className={classes.tableWrapper}>
+          <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+                <CustomTableCell>SPONSOR</CustomTableCell>
+                <CustomTableCell>STUDY</CustomTableCell>
+                <CustomTableCell>FROM</CustomTableCell>
+                <CustomTableCell>TO</CustomTableCell>
+                <CustomTableCell numeric>COMPLETED FAXES</CustomTableCell>
+                <CustomTableCell numeric>COMPLETED (US FAXES)</CustomTableCell>
+                <CustomTableCell numeric>COMPLETED (INT. FAXES)</CustomTableCell>
+                <CustomTableCell numeric>PROJECT CODE</CustomTableCell>
+            </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(n => {
+                return (
+                    <TableRow key={n.id}>
+                    <TableCell component="th" scope="row">
+                      {n.sponsor}
+                    </TableCell>
+                    <TableCell>{n.study}</TableCell>
+                    <TableCell>{n.from}</TableCell>
+                    <TableCell>{n.to}</TableCell>
+                    <TableCell numeric>{n.completed}</TableCell>
+                    <TableCell numeric>{n.completed_us}</TableCell>
+                    <TableCell numeric>{n.completed_int}</TableCell>
+                    <TableCell numeric>{n.project_code}</TableCell>
+                  </TableRow>
+                );
+              })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 48 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  colSpan={8}
+                  count={rows.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onChangePage={this.handleChangePage}
+                  onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActionsWrapped}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </div>
+      </Paper>
+    );
+  }
+}
+
+CustomPaginationActionsTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CustomPaginationActionsTable);
--- src/views/Reports.js
+++ src/views/Reports.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import TotalFaxReport from '../TotalFaxReport';
+import TotalFaxReport from '../report';
 import TotalFaxSearch from '../TotalFaxSearch';
 
 const Reports = () => (
